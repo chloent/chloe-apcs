cc877bf0c265c725e2f3df4ccc9a9c5ddc243cdd
chloent <chloetjo26@marlborough.org>
2025-09-11 13:26:27 -0700

    unit 1 exercises

---
+++ unit1/geometric.java
@@ -0,0 +1,7 @@
+public class gemoetric 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ unit1/multiples.java
@@ -0,0 +1,7 @@
+public class multiples 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+}
