cb625be9ea5770f4e9c00b93f78e8b30f0fbd370
chloent <chloetjo26@marlborough.org>
2025-09-15 16:35:20 -0700

    fizzbuzz

--- unit1/FizzBuzz.java
+++ unit1/FizzBuzz.java
@@ -2,6 +2,29 @@ public class FizzBuzz
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, world!");
+        for (int i=0; i<=30; i++)
+        {
+			if (i%3 == 0 && i%5 == 0)
+			{ 
+				System.out.println("FizzBuzz"); 
+				
+			}
+			else if (i%3 == 0)
+			{ 
+				System.out.println("Fizz"); 
+			}
+			else if (i%5 == 0)
+			{
+				System.out.println("Buzz"); 
+			}
+			
+			else 
+			{
+				System.out.println(i);
+		
+
+		}
     }
+ 
+}
 }
--- unit1/multiples.java
+++ unit1/multiples.java
@@ -3,7 +3,7 @@ public class Multiples
     public static void main(String[] args)
     {
         System.out.println("Multiples of 3");        
-        for (int i=0; i<=36; i++)
+        for (int i=0; i<=30; i++)
         { 
 			if (i%3 == 0) {
 			System.out.println(i); 
